c29953719374b8aeda25420c912d1a5a8679c54f
Martin Nyhus <martin.nyhus@gmx.com>
2011-03-25 00:58:09 +0100

    Add test for storing message flags

---
+++ test/freemail/MailMessageTest.java
@@ -0,0 +1,72 @@
+/*
+ * MailMessageTest.java
+ * This file is part of Freemail, copyright (C) 2011 Martin Nyhus
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package freemail;
+
+import java.io.File;
+import java.io.IOException;
+
+import utils.Utils;
+
+import junit.framework.TestCase;
+
+public class MailMessageTest extends TestCase {
+	private static final String MESSAGE_DIR = "msg_dir";
+
+	private File msgDir = null;
+
+	public void setUp() {
+		// Create a directory for messages so it is easier to list files, clean up etc.
+		msgDir = new File(MESSAGE_DIR);
+		if(msgDir.exists()) {
+			System.out.println("WARNING: Message directory exists, deleting");
+			Utils.delete(msgDir);
+		}
+
+		if(!msgDir.mkdir()) {
+			System.out.println("WARNING: Could not create message directory, tests will probably fail");
+		}
+	}
+
+	public void tearDown() {
+		Utils.delete(msgDir);
+	}
+
+	/*
+	 * Test for the bug that was fixed in add3f39743a303a748813666f1e1de6a25ca29aa. MailMessage
+	 * would lose track of the file when storing a different set of flags, so the second attempt
+	 * would fail silently.
+	 */
+	public void testStoreFlagsTwice() throws IOException {
+		File messageFile = new File(msgDir, "0");
+		messageFile.createNewFile();
+
+		//Create new message and clear flags in case any were set
+		MailMessage msg = new MailMessage(messageFile, 0);
+		msg.flags.clear();
+
+		msg.flags.set("\\Seen", true);
+		msg.storeFlags();
+		assertEquals(new File(msgDir, "0,S"), msgDir.listFiles()[0]);
+
+		msg.flags.set("\\Deleted", true);
+		msg.storeFlags();
+		assertEquals(new File(msgDir, "0,SX"), msgDir.listFiles()[0]);
+	}
+}
