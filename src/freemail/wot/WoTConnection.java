a76986be33e299ef9e8eb4f365bd99d0fd9ff0ba
Martin Nyhus <martin.nyhus@gmx.com>
2011-05-16 13:57:29 +0200

    Add method for sending a blocking message

--- src/freemail/wot/WoTConnection.java
+++ src/freemail/wot/WoTConnection.java
@@ -13,12 +13,57 @@ public class WoTConnection implements FredPluginTalker {
 
 	private final PluginTalker pluginTalker;
 
+	private Message reply = null;
+	private final Object replyLock = new Object();
+
 	public WoTConnection(PluginRespirator pr) throws PluginNotFoundException {
 		pluginTalker = pr.getPluginTalker(this, WOT_PLUGIN_NAME, CONNECTION_IDENTIFIER);
 	}
 
 	@Override
 	public void onReply(String pluginname, String indentifier, SimpleFieldSet params, Bucket data) {
-		assert(false); //Unsupported for now
+		synchronized(replyLock) {
+			assert reply == null : "Reply should be null, but was " + reply;
+
+			reply = new Message(params, data);
+		}
+	}
+
+	private Message sendBlocking(final Message msg) {
+		//Synchronize on pluginTalker so once on message can be sent at a time
+		synchronized(pluginTalker) {
+			pluginTalker.send(msg.sfs, msg.data);
+
+			final Message retValue;
+			synchronized(replyLock) {
+				while(reply == null) {
+					try {
+						replyLock.wait();
+					} catch (InterruptedException e) {
+						//Just check again
+					}
+				}
+
+				retValue = reply;
+				reply = null;
+			}
+
+			return retValue;
+		}
+	}
+
+	private static class Message {
+		private final SimpleFieldSet sfs;
+		private final Bucket data;
+
+		private Message(SimpleFieldSet sfs, Bucket data) {
+			this.sfs = sfs;
+			this.data = data;
+		}
+
+		@Override
+		public String toString() {
+			return "[" + sfs + "] [" + data + "]";
+		}
 	}
 }
