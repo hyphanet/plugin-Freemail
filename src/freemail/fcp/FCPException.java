78ec931ac56ed9a193d05e575164ac9bbcfc58e4
Martin Nyhus <martin.nyhus@gmx.com>
2011-05-27 12:59:19 +0200

    Read description fields from FCPMessage

--- src/freemail/fcp/FCPException.java
+++ src/freemail/fcp/FCPException.java
@@ -23,22 +23,37 @@ public class FCPException extends Exception {
 	public final int errorcode;
 	public final boolean isFatal;
 
+	private final String codeDescription;
+	private final String extraDescription;
+	private final String shortCodeDescription;
+
 	FCPException(FCPMessage msg) {
+		//Read the error code
 		String code = (String)msg.headers.get("Code");
 		if (code != null)
 			this.errorcode = Integer.parseInt(code);
 		else
 			this.errorcode = 0;
 
+		//Read the fatal field
 		String fatal = (String)msg.headers.get("Fatal");
 		if (fatal != null)
 			this.isFatal = (fatal.equalsIgnoreCase("true"));
 		else
 			this.isFatal = false;
+
+		//Read the description fields
+		this.codeDescription = (String)msg.headers.get("CodeDescription");
+		this.extraDescription = (String)msg.headers.get("ExtraDescription");
+		this.shortCodeDescription = (String)msg.headers.get("ShortCodeDescription");
 	}
 
 	FCPException(int code, boolean fatal) {
 		errorcode = code;
 		isFatal = fatal;
+
+		this.codeDescription = null;
+		this.extraDescription = null;
+		this.shortCodeDescription = null;
 	}
 }
