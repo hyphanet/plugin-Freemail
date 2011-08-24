hyphanet/plugin-Freemail

commit b4afb3102b5aba7a204358655fa2e8b82cace682

Remove extra / from CSSToadlet path

src/freemail/ui/web/CSSToadlet.java
@@ -33,7 +33,7 @@ import freenet.support.api.HTTPRequest;
 import freenet.support.io.BucketTools;
 
 public class CSSToadlet extends WebPage {
-	private static final String PATH = WebInterface.PATH + "/static/css/";
+	private static final String PATH = WebInterface.PATH + "/static/css";
 
 	CSSToadlet(PluginRespirator pluginRespirator) {
 		super(pluginRespirator);

commit 1fc6216580d85289bf707e589427b6ae571854c0

Fix CSS page that broke in b4afb310

src/freemail/ui/web/CSSToadlet.java
@@ -46,7 +46,7 @@ public class CSSToadlet extends WebPage {
 
 	@Override
 	void makeWebPageGet(URI uri, HTTPRequest req, ToadletContext ctx, PageNode page) throws ToadletContextClosedException, IOException {
-		String filename = uri.getPath().substring(PATH.length());
+		String filename = uri.getPath().substring(PATH.length() + "/".length());
 
 		//Check that the filename has the expected format
 		if(!filename.matches("[a-zA-Z0-9]+\\.css")) {