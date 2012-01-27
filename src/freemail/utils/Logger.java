fe5885a1d3d931b697b506997f1df49f1e188db7
Martin Nyhus <martin.nyhus@gmx.com>
2012-01-28 00:37:39 +0100

    Clean up Logger (formatting, indentation etc.)

--- src/freemail/utils/Logger.java
+++ src/freemail/utils/Logger.java
@@ -19,15 +19,6 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-/*
- * Logger class for Freemail
- *
- * this is a first attempt at fixing the logging so that not everything
- * is written to stdout. This class attempts to mimic the Logger class
- * from Freenet and calls the Freenet Logger class is available.
- *
- */
-
 package freemail.utils;
 
 import java.lang.NoClassDefFoundError;
@@ -37,133 +28,134 @@ import java.util.Date;
 import freemail.config.ConfigClient;
 import freenet.support.Logger.LogLevel;
 
+/**
+ * Logger class for Freemail. This is a first attempt at fixing the logging so
+ * that not everything is written to stdout. This class attempts to mimic the
+ * Logger class from Freenet and calls the Freenet Logger class is available.
+ */
 public class Logger implements ConfigClient {
+	private static final int INTERNAL = 1;
+	private static final int DEBUG = 2;
+	private static final int MINOR = 4;
+	private static final int NORMAL = 8;
+	private static final int ERROR = 16;
 
-	static final private int INTERNAL=1;
-	static final private int DEBUG=2;
-	static final private int MINOR=4;
-	static final private int NORMAL=8;
-	static final private int ERROR=16;
-	
-	static boolean initialized=false;
-	static boolean foundFreenetLogger=false;
-	
-	//static final private int loglevel=INTERNAL|DEBUG|MINOR|NORMAL|ERROR; // everything
-	//static final private int loglevel=DEBUG|NORMAL|ERROR;
-	static private int loglevel=NORMAL|ERROR; // should be ok for normal users
-	
-	static private SimpleDateFormat logDateFormat = new SimpleDateFormat("d/MM/yyyy HH:mm:ss");
-	
+	static boolean initialized = false;
+	static boolean foundFreenetLogger = false;
+
+	// static final private int loglevel=INTERNAL|DEBUG|MINOR|NORMAL|ERROR; // everything
+	// static final private int loglevel=DEBUG|NORMAL|ERROR;
+	private static int loglevel = NORMAL | ERROR; // should be ok for normal users
+
+	private static SimpleDateFormat logDateFormat = new SimpleDateFormat("d/MM/yyyy HH:mm:ss");
+
 	@Override
 	public void setConfigProp(String key, String val) {
-		if (key.equals("loglevel")) {
+		if(key.equals("loglevel")) {
 			String[] levels = val.split("\\s*\\|\\s*");
-			
+
 			loglevel = 0;
-			
+
 			for (int i = 0; i < levels.length; i++) {
-				if (levels[i].equalsIgnoreCase("internal")) {
+				if(levels[i].equalsIgnoreCase("internal")) {
 					loglevel |= INTERNAL;
-				} else if (levels[i].equalsIgnoreCase("debug")) {
+				} else if(levels[i].equalsIgnoreCase("debug")) {
 					loglevel |= DEBUG;
-				} else if (levels[i].equalsIgnoreCase("minor")) {
+				} else if(levels[i].equalsIgnoreCase("minor")) {
 					loglevel |= MINOR;
-				} else if (levels[i].equalsIgnoreCase("normal")){
+				} else if(levels[i].equalsIgnoreCase("normal")) {
 					loglevel |= NORMAL;
-				} else if (levels[i].equalsIgnoreCase("error")){
+				} else if(levels[i].equalsIgnoreCase("error")) {
 					loglevel |= ERROR;
 				}
 			}
 		}
 	}
 
-	static private boolean useFreenetLogger()
-	{
+	private static boolean useFreenetLogger() {
 		if(!initialized) {
 			try {
 				freenet.support.Logger.shouldLog(LogLevel.ERROR, null);
-				foundFreenetLogger=true;
+				foundFreenetLogger = true;
+			} catch (NoClassDefFoundError ex) {
+				foundFreenetLogger = false;
 			}
-			catch(NoClassDefFoundError ex) {
-				foundFreenetLogger=false;
-			}
-			initialized=true;
+			initialized = true;
 		}
 		return foundFreenetLogger;
 	}
-	
-	static private void log(int l, Object o, String s, String level) {
+
+	private static void log(int l, Object o, String s, String level) {
 		log(l, o.getClass(), s, level);
 	}
 
-	static private synchronized void log(int l, Class<?> c, String s, String level) {
-		if((l&loglevel)!=0) {
-			System.err.println(logDateFormat.format(new Date())+" "+level+"("+c.getName()+"): "+s);
+	private static synchronized void log(int l, Class<?> c, String s, String level) {
+		if((l & loglevel) != 0) {
+			System.err.println(logDateFormat.format(new Date()) + " " + level + "(" + c.getName() + "): " + s);
 		}
 	}
 
-	static public void minor(Object o, String s) {
+	public static void minor(Object o, String s) {
 		if(useFreenetLogger()) {
-			freenet.support.Logger.minor(o,s);
+			freenet.support.Logger.minor(o, s);
 		} else {
-			log(MINOR,o,s,"MINOR");
+			log(MINOR, o, s, "MINOR");
 		}
 	}
 
-	static public void minor(Class<?> c, String s) {
+	public static void minor(Class<?> c, String s) {
 		if(useFreenetLogger()) {
-			freenet.support.Logger.minor(c,s);
+			freenet.support.Logger.minor(c, s);
 		} else {
-			log(MINOR,c,s,"MINOR");
+			log(MINOR, c, s, "MINOR");
 		}
 	}
 
-	static public void normal(Object o, String s) {
+	public static void normal(Object o, String s) {
 		if(useFreenetLogger()) {
-			freenet.support.Logger.normal(o,s);
+			freenet.support.Logger.normal(o, s);
 		} else {
-			log(NORMAL,o,s,"NORMAL");
+			log(NORMAL, o, s, "NORMAL");
 		}
 	}
 
-	static public void normal(Class<?> c, String s) {
+	public static void normal(Class<?> c, String s) {
 		if(useFreenetLogger()) {
-			freenet.support.Logger.normal(c,s);
+			freenet.support.Logger.normal(c, s);
 		} else {
-			log(NORMAL,c,s,"NORMAL");
+			log(NORMAL, c, s, "NORMAL");
 		}
 	}
 
-	static public void error(Object o, String s) {
+	public static void error(Object o, String s) {
 		if(useFreenetLogger()) {
-			freenet.support.Logger.error(o,s);
+			freenet.support.Logger.error(o, s);
 		} else {
-			log(ERROR,o,s,"ERROR");
+			log(ERROR, o, s, "ERROR");
 		}
 	}
 
-	static public void error(Class<?> c, String s) {
+	public static void error(Class<?> c, String s) {
 		if(useFreenetLogger()) {
-			freenet.support.Logger.error(c,s);
+			freenet.support.Logger.error(c, s);
 		} else {
-			log(ERROR,c,s,"ERROR");
+			log(ERROR, c, s, "ERROR");
 		}
 	}
 
-	static public void debug(Object o, String s) {
+	public static void debug(Object o, String s) {
 		if(useFreenetLogger()) {
-			freenet.support.Logger.debug(o,s);
+			freenet.support.Logger.debug(o, s);
 		} else {
-			log(DEBUG,o,s,"DEBUG");
+			log(DEBUG, o, s, "DEBUG");
 		}
 	}
 
-	static public void debug(Class<?> c, String s) {
+	public static void debug(Class<?> c, String s) {
 		if(useFreenetLogger()) {
-			freenet.support.Logger.debug(c,s);
+			freenet.support.Logger.debug(c, s);
 		} else {
-			log(DEBUG,c,s,"DEBUG");
+			log(DEBUG, c, s, "DEBUG");
 		}
 	}
-
 }
