8ac26c096bbb0ef945aaca1dbfa9eb193dd79d47
Martin Nyhus <martin.nyhus@gmx.com>
2012-02-20 16:09:48 +0100

    Add test for append with custom flag
    Even though the custom flag is included in the flags list the \Seen flag
    should still be saved.

--- test/freemail/imap/IMAPAppendTest.java
+++ test/freemail/imap/IMAPAppendTest.java
@@ -5,6 +5,8 @@ import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
+import junit.framework.ComparisonFailure;
+
 public class IMAPAppendTest extends IMAPTestBase {
 	private static final List<String> INITIAL_RESPONSES;
 	static {
@@ -56,6 +58,46 @@ public class IMAPAppendTest extends IMAPTestBase {
 		expectedResponse.add("0004 OK Fetch completed");
 
 		runSimpleTest(commands, expectedResponse);
+	}
+
+	/*
+	 * The expectation here is that the custom flag is ignored since they aren't supported, but the
+	 * \Seen flag should still be saved.
+	 */
+	public void testAppendWithCustomFlag() throws IOException {
+		List<String> commands = new LinkedList<String>();
+		commands.add("0001 LOGIN " + USERNAME + " test");
+		commands.add("0002 SELECT INBOX");
+		commands.add("0003 APPEND INBOX (\\Seen custom) {23}");
+		commands.add("Subject: Test message");
+		commands.add("0004 UID FETCH 10:* FLAGS");
+
+		List<String> expectedResponse = new LinkedList<String>();
+		expectedResponse.addAll(INITIAL_RESPONSES);
+		expectedResponse.add("+ OK");
+		expectedResponse.add("0003 OK APPEND completed");
+		expectedResponse.add("* 10 FETCH (FLAGS () UID 10)");
+		expectedResponse.add("* 11 FETCH (FLAGS (\\Seen \\Recent) UID 11)");
+		expectedResponse.add("0004 OK Fetch completed");
+
+		try {
+			runSimpleTest(commands, expectedResponse);
+			fail("Append with custom flags appear to work, fix this " +
+			     "test so regressions will cause the test to fail");
+		} catch(ComparisonFailure e) {
+			/*
+			 * A test failure is expected at the moment since the bug hasn't
+			 * been fixed yet. Check that the expected and actual values don't
+			 * change and print a warning.
+			 */
+			final String expected = "* 11 FETCH (FLAGS (\\Seen \\Recent) UID 11)";
+			final String actual = "* 11 FETCH (FLAGS (\\Recent) UID 11)";
+
+			assertEquals(expected, e.getExpected());
+			assertEquals(actual, e.getActual());
+
+			System.err.println("testAppendWithCustomFlag: Expected failure");
+		}
 	}
 
 	public void testAppendWithFlagAndDate() throws IOException {
