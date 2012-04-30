fc62326402afcfba9f41fd53b11828435786d75b
Martin Nyhus <martin.nyhus@gmx.com>
2012-04-30 18:20:02 +0200

    Freemail 0.2

--- src/org/freenetproject/freemail/Version.java
+++ src/org/freenetproject/freemail/Version.java
@@ -23,14 +23,14 @@ package org.freenetproject.freemail;
 
 public class Version {
 	/** The human readable version */
-	public static final String VERSION = "0.1.1";
+	public static final String VERSION = "0.2";
 
 	/**
 	 * The build number, used by the plugin auto-updater. This must always
 	 * increase, and at least by one per build that is uploaded to the auto
 	 * update system.
 	 */
-	public static final int BUILD_NO = 15;
+	public static final int BUILD_NO = 16;
 
 	/** Version number updated at build time using git describe */
 	public static final String GIT_REVISION = "@custom@";
