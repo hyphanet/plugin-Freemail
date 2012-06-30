9ff37f16ff19f3a8d9fa09ddc6dc6ad81711ba67
Martin Nyhus <martin.nyhus@gmx.com>
2012-06-30 18:37:46 +0200

    IMAP: Add test for uid store without arguments

---
+++ test/org/freenetproject/freemail/imap/IMAPUidStoreTest.java
@@ -0,0 +1,54 @@
+/*
+ * IMAPUidStoreTest.java
+ * This file is part of Freemail, copyright (C) 2012
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package org.freenetproject.freemail.imap;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class IMAPUidStoreTest extends IMAPTestWithMessages {
+	private static final List<String> INITIAL_RESPONSES;
+	static {
+		List<String> backing = new LinkedList<String>();
+		backing.add("* OK [CAPABILITY IMAP4rev1 CHILDREN NAMESPACE] Freemail ready - hit me with your rhythm stick.");
+		backing.add("0001 OK Logged in");
+		backing.add("* FLAGS (\\Seen \\Answered \\Flagged \\Deleted \\Draft \\Recent)");
+		backing.add("* OK [PERMANENTFLAGS (\\Seen \\Answered \\Flagged \\Deleted \\Draft \\Recent)] Limited");
+		backing.add("* 10 EXISTS");
+		backing.add("* 10 RECENT");
+		backing.add("* OK [UIDVALIDITY 1] Ok");
+		backing.add("0002 OK [READ-WRITE] Done");
+		INITIAL_RESPONSES = Collections.unmodifiableList(backing);
+	}
+
+	public void testUidStoreWithoutArguments() throws IOException {
+		List<String> commands = new LinkedList<String>();
+		commands.add("0001 LOGIN " + IMAP_USERNAME + " test");
+		commands.add("0002 SELECT INBOX");
+		commands.add("0003 UID STORE");
+
+		List<String> expectedResponse = new LinkedList<String>();
+		expectedResponse.addAll(INITIAL_RESPONSES);
+		expectedResponse.add("0003 BAD Not enough arguments to uid command");
+
+		runSimpleTest(commands, expectedResponse);
+	}
+}
