b1f54b70393c096f25c53bf0cbb5d3c7fd698cdb
Martin Nyhus <martin.nyhus@gmx.com>
2012-06-30 14:12:38 +0200

    IMAP: Add fetch tests for range containing 0

--- test/org/freenetproject/freemail/imap/IMAPFetchTest.java
+++ test/org/freenetproject/freemail/imap/IMAPFetchTest.java
@@ -279,4 +279,30 @@ public class IMAPFetchTest extends IMAPTestWithMessages {
 
 		runSimpleTest(commands, expectedResponse);
 	}
+
+	public void testFetchWithRangeFrom0() throws IOException {
+		List<String> commands = new LinkedList<String>();
+		commands.add("0001 LOGIN " + IMAP_USERNAME + " test");
+		commands.add("0002 SELECT inbox");
+		commands.add("0003 FETCH 0:10 UID");
+
+		List<String> expectedResponse = new LinkedList<String>();
+		expectedResponse.addAll(INITIAL_RESPONSES);
+		expectedResponse.add("0003 NO Invalid message ID");
+
+		runSimpleTest(commands, expectedResponse);
+	}
+
+	public void testFetchWithRangeTo0() throws IOException {
+		List<String> commands = new LinkedList<String>();
+		commands.add("0001 LOGIN " + IMAP_USERNAME + " test");
+		commands.add("0002 SELECT inbox");
+		commands.add("0003 FETCH 10:0 UID");
+
+		List<String> expectedResponse = new LinkedList<String>();
+		expectedResponse.addAll(INITIAL_RESPONSES);
+		expectedResponse.add("0003 NO Invalid message ID");
+
+		runSimpleTest(commands, expectedResponse);
+	}
 }
