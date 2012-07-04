c0c5a056f541795409d37b3bb06d6dc6260c7a06
Martin Nyhus <martin.nyhus@gmx.com>
2012-07-04 20:45:02 +0200

    Fold the References header to avoid long lines

--- src/org/freenetproject/freemail/ui/web/NewMessageToadlet.java
+++ src/org/freenetproject/freemail/ui/web/NewMessageToadlet.java
@@ -365,7 +365,14 @@ public class NewMessageToadlet extends WebPage {
 
 		List<String> extraHeaders = readExtraHeaders(req);
 		extraHeaders.add("In-Reply-To: " + msg.getFirstHeader("message-id"));
-		extraHeaders.add("References: " + msg.getFirstHeader("References") + msg.getFirstHeader("message-id"));
+
+		//Add the references header. This uses folding white space between each
+		//reference as a simple way of avoiding long lines.
+		String references = msg.getFirstHeader("References") + msg.getFirstHeader("message-id");
+		extraHeaders.add("References:");
+		for(String part : references.split(" ")) {
+			extraHeaders.add(" " + part);
+		}
 
 		HTMLNode messageBox = addInfobox(contentNode, FreemailL10n.getString("Freemail.NewMessageToadlet.boxTitle"));
 		addMessageForm(messageBox, ctx, Collections.singletonList(recipient), subject,
