2361dd29dcb90b703f5ee6d9783a5fc7bb30b91c
Martin Nyhus <martin.nyhus@gmx.com>
2012-07-05 22:59:48 +0200

    IMAP: Add tests for uid store with bad ranges

--- test/org/freenetproject/freemail/imap/IMAPUidStoreTest.java
+++ test/org/freenetproject/freemail/imap/IMAPUidStoreTest.java
@@ -90,4 +90,30 @@ public class IMAPUidStoreTest extends IMAPTestWithMessages {
 
 		runSimpleTest(commands, expectedResponse);
 	}
+
+	public void testUidStoreWithBadRangeStart() throws IOException {
+		List<String> commands = new LinkedList<String>();
+		commands.add("0001 LOGIN " + IMAP_USERNAME + " test");
+		commands.add("0002 SELECT INBOX");
+		commands.add("0003 UID STORE BAD:12 +FLAGS (\\Seen \\Flagged \\Answered");
+
+		List<String> expectedResponse = new LinkedList<String>();
+		expectedResponse.addAll(INITIAL_RESPONSES);
+		expectedResponse.add("0003 BAD Bad number");
+
+		runSimpleTest(commands, expectedResponse);
+	}
+
+	public void testUidStoreWithBadRangeEnd() throws IOException {
+		List<String> commands = new LinkedList<String>();
+		commands.add("0001 LOGIN " + IMAP_USERNAME + " test");
+		commands.add("0002 SELECT INBOX");
+		commands.add("0003 UID STORE 11:BAD +FLAGS (\\Seen \\Flagged \\Answered");
+
+		List<String> expectedResponse = new LinkedList<String>();
+		expectedResponse.addAll(INITIAL_RESPONSES);
+		expectedResponse.add("0003 BAD Bad number");
+
+		runSimpleTest(commands, expectedResponse);
+	}
 }
