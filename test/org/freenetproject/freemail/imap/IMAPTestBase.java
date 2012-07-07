78863d873b98d0f7fd537ed593eb30d496083da8
Martin Nyhus <martin.nyhus@gmx.com>
2012-07-08 00:36:55 +0200

    IMAP: Add 1 second timeout to most tests

--- test/org/freenetproject/freemail/imap/IMAPTestBase.java
+++ test/org/freenetproject/freemail/imap/IMAPTestBase.java
@@ -25,9 +25,12 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.io.Reader;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import org.freenetproject.freemail.AccountManager;
 import org.freenetproject.freemail.imap.IMAPHandler;
@@ -107,6 +110,14 @@ public abstract class IMAPTestBase extends TestCase {
 		try {
 			int lineNum = 0;
 			for(String response : expectedResponse) {
+				try {
+					waitForReady(fromHandler, 1, TimeUnit.SECONDS);
+				} catch (TimeoutException e) {
+					fail("Test timed out waiting for response");
+				} catch (InterruptedException e) {
+					fail("Thread interrupted unexpectedly");
+				}
+
 				String line = fromHandler.readLine();
 				assertEquals("Failed at line " + lineNum++, response, line);
 			}
@@ -120,6 +131,24 @@ public abstract class IMAPTestBase extends TestCase {
 			} catch(InterruptedException e) {
 				fail("Caught unexpected InterruptedException");
 			}
+		}
+	}
+
+	private void waitForReady(Reader reader, int timeout, TimeUnit unit)
+			throws IOException, TimeoutException, InterruptedException {
+		long start = System.nanoTime();
+		int sleepTime = 1;
+
+		while(!reader.ready()) {
+			//Check if we have timed out
+			long waited = System.nanoTime() - start;
+			if(unit.convert(waited, TimeUnit.NANOSECONDS) > timeout) {
+				throw new TimeoutException();
+			}
+
+			//If not sleep a little
+			Thread.sleep(sleepTime);
+			sleepTime = Math.max(sleepTime * 2, 100);
 		}
 	}
 }
