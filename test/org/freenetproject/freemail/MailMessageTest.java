881fe1e16cf90d186d654781d9f95357b0a0c827
Martin Nyhus <martin.nyhus@gmx.com>
2012-07-15 21:08:23 +0200

    Add two simple tests for body decoding

--- test/org/freenetproject/freemail/MailMessageTest.java
+++ test/org/freenetproject/freemail/MailMessageTest.java
@@ -19,6 +19,7 @@
 
 package org.freenetproject.freemail;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -146,5 +147,45 @@ public class MailMessageTest extends TestCase {
 			String decoded = MailMessage.decodeHeader(encoded);
 			assertEquals(s, decoded);
 		}
+	}
+
+	public void testDecodeQpAndAsciiMessageBody() throws IOException {
+		File messageFile = new File(msgDir, "0");
+		messageFile.createNewFile();
+
+		PrintWriter pw = new PrintWriter(messageFile);
+		pw.print("Content-Transfer-Encoding: quoted-printable\r\n");
+		pw.print("Content-Type: text/plain; charset=us-ascii\r\n");
+		pw.print("\r\n");
+		pw.print("Test message, line 1\r\n");
+		pw.print("Test message, line 2\r\n");
+		pw.close();
+
+		MailMessage msg = new MailMessage(messageFile, 0);
+		BufferedReader reader = msg.getBodyReader();
+
+		assertEquals("Test message, line 1", reader.readLine());
+		assertEquals("Test message, line 2", reader.readLine());
+		assertEquals(null, reader.readLine());
+	}
+
+	public void testDecodeQpAndUtf8MessageBody() throws IOException {
+		File messageFile = new File(msgDir, "0");
+		messageFile.createNewFile();
+
+		PrintWriter pw = new PrintWriter(messageFile);
+		pw.print("Content-Transfer-Encoding: quoted-printable\r\n");
+		pw.print("Content-Type: text/plain; charset=utf-8\r\n");
+		pw.print("\r\n");
+		pw.print("Test message (=C3=A6), line 1\r\n");
+		pw.print("Test message (=C3=A6), line 2\r\n");
+		pw.close();
+
+		MailMessage msg = new MailMessage(messageFile, 0);
+		BufferedReader reader = msg.getBodyReader();
+
+		assertEquals("Test message (æ), line 1", reader.readLine());
+		assertEquals("Test message (æ), line 2", reader.readLine());
+		assertEquals(null, reader.readLine());
 	}
 }
