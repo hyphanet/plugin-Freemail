842da55db3b5d4155767f1ca233a15e11e72580c
Martin Nyhus <martin.nyhus@gmx.com>
2012-07-29 02:58:13 +0200

    Add a few unit tests for message body encoding

--- test/org/freenetproject/freemail/MailMessageTest.java
+++ test/org/freenetproject/freemail/MailMessageTest.java
@@ -20,8 +20,10 @@
 package org.freenetproject.freemail;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.LinkedList;
@@ -224,5 +226,109 @@ public class MailMessageTest extends TestCase {
 
 		assertEquals("Test message (æøå), line 1 was split across two lines", reader.readLine());
 		assertEquals(null, reader.readLine());
+	}
+
+	public void testEncodeAsciiText() throws IOException {
+		byte[] input = "Test message 123".getBytes();
+		runEncoderTest(input, input);
+	}
+
+	public void testEncodeTrailingLineBreak() throws IOException {
+		byte[] input = "Test message\r\n".getBytes();
+		runEncoderTest(input, input);
+	}
+
+	public void testEncodeEOLWhitespace() throws IOException {
+		byte[] input = "Test message \r\n".getBytes();
+		byte[] expected = "Test message=20\r\n".getBytes();
+		runEncoderTest(expected, input);
+	}
+
+	public void testEncodeUTF8() throws IOException {
+		byte[] input = "æ∀\r\n".getBytes();
+		byte[] expected = "=C3=A6=E2=88=80\r\n".getBytes();
+		runEncoderTest(expected, input);
+	}
+
+	public void testEncodeSingleCarriageReturn() throws IOException {
+		byte[] input = "Test\r\r\n".getBytes();
+		byte[] expected = "Test=0D\r\n".getBytes();
+		runEncoderTest(expected, input);
+	}
+
+	public void testEncodeSingleNewline() throws IOException {
+		byte[] input = "Test\n\r\n".getBytes();
+		byte[] expected = "Test=0A\r\n".getBytes();
+		runEncoderTest(expected, input);
+	}
+
+	public void testEncodeLongLine() throws IOException {
+		byte[] input = (
+				"Test of a long line that will require a soft line break because it is more "
+				+ "than 78 characters long").getBytes();
+		byte[] expected = (
+				"Test of a long line that will require a soft line break because it is more =\r\n"
+				+ "than 78 characters long").getBytes();
+		runEncoderTest(expected, input);
+	}
+
+	public void testEncodeEquals() throws IOException {
+		byte[] input = "Test=message\r\n".getBytes();
+		byte[] expected = "Test=3Dmessage\r\n".getBytes();
+		runEncoderTest(expected, input);
+	}
+
+	public void testEncodeAsciiDel() throws IOException {
+		byte[] input = "Test?message\r\n".getBytes();
+		input[4] = 0x7F;
+
+		byte[] expected = "Test=7Fmessage\r\n".getBytes();
+		runEncoderTest(expected, input);
+	}
+
+	public void testEncodeTab() throws IOException {
+		byte[] input = "Test\tmessage\r\n".getBytes();
+		byte[] expected = "Test\tmessage\r\n".getBytes();
+		runEncoderTest(expected, input);
+	}
+
+	public void testEncodeTabAtEOL() throws IOException {
+		byte[] input = "Test message\t\r\n".getBytes();
+		byte[] expected = "Test message=09\r\n".getBytes();
+		runEncoderTest(expected, input);
+	}
+
+	public void testEncodeSpaceAndNewline() throws IOException {
+		byte[] input = " \n\r\n".getBytes();
+		byte[] expected = " =0A\r\n".getBytes();
+		runEncoderTest(expected, input);
+	}
+
+	public void testBufferExpansionWhileEncoding() throws IOException {
+		byte[] input = "          ".getBytes();
+		byte[] expected = "         =20".getBytes();
+		runEncoderTest(expected, input);
+	}
+
+	public void testInsertSoftLineBreakBeforeEncodedChar() throws IOException {
+		byte[] input = "Test of a long line that will require a soft line before the encoded char: æ".getBytes();
+		byte[] expected =
+				"Test of a long line that will require a soft line before the encoded char: =\r\n=C3=A6".getBytes();
+		runEncoderTest(expected, input);
+	}
+
+	private void runEncoderTest(byte[] expected, byte[] input) throws IOException {
+		for(int i = 0; i < expected.length; i++) {
+			if(expected[i] >= 0x80) {
+				fail("Expected output can't contain 8bit characters");
+			}
+		}
+
+		ByteArrayOutputStream output = new ByteArrayOutputStream();
+		OutputStream encoder = new MailMessage.EncodingOutputStream(output);
+		encoder.write(input);
+		encoder.close();
+
+		Utils.assertEquals(expected, output.toByteArray());
 	}
 }
