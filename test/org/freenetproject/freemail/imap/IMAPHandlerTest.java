ac63a9a277ec1183c018bf82f8597ef4f8620c10
Martin Nyhus <martin.nyhus@gmx.com>
2012-08-02 22:22:33 +0200

    Fix expected response for uid without arguments

--- test/org/freenetproject/freemail/imap/IMAPHandlerTest.java
+++ test/org/freenetproject/freemail/imap/IMAPHandlerTest.java
@@ -401,7 +401,7 @@ public class IMAPHandlerTest extends IMAPTestWithMessages {
 
 		List<String> expectedResponse = new LinkedList<String>();
 		expectedResponse.addAll(INITIAL_RESPONSES);
-		expectedResponse.add("0003 BAD Not enough arguments to uid command");
+		expectedResponse.add("0003 BAD Not enough arguments for uid command");
 
 		runSimpleTest(commands, expectedResponse);
 	}
