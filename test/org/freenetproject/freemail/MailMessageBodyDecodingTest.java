fb67517ab67d50c39fed12100930109ec03953f5
Martin Nyhus <martin.nyhus@gmx.com>
2012-08-21 23:03:23 +0200

    Add base64 test for line buffering

--- test/org/freenetproject/freemail/MailMessageBodyDecodingTest.java
+++ test/org/freenetproject/freemail/MailMessageBodyDecodingTest.java
@@ -169,4 +169,25 @@ public class MailMessageBodyDecodingTest extends TestCase {
 		assertEquals("Test message (æ), line 2", reader.readLine());
 		assertEquals(null, reader.readLine());
 	}
+
+	public void testDecodeBase64WithLineBuffering() throws IOException {
+		File messageFile = new File(msgDir, "0");
+		messageFile.createNewFile();
+
+		PrintWriter pw = new PrintWriter(messageFile);
+		pw.print("Content-Transfer-Encoding: base64\r\n");
+		pw.print("Content-Type: text/plain; charset=us-ascii\r\n");
+		pw.print("\r\n");
+		pw.print("VGVzdCBtZXNzYWdlLCBsaW5lIDENClRlc3QgbWVzc2FnZSwgbGlu"
+				+ "ZSAyDQpUZXN0IG1lc3NhZ2UsIGxpbmUgMw0K\r\n");
+		pw.close();
+
+		MailMessage msg = new MailMessage(messageFile, 0);
+		BufferedReader reader = msg.getBodyReader();
+
+		assertEquals("Test message, line 1", reader.readLine());
+		assertEquals("Test message, line 2", reader.readLine());
+		assertEquals("Test message, line 3", reader.readLine());
+		assertEquals(null, reader.readLine());
+	}
 }
