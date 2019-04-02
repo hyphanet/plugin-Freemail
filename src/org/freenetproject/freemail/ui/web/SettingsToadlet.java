64f1ed992be8d3e49cb671151eb346296e605510
devalexk <devalexk@github.com>
2019-04-02 17:11:33 +0300

    Validating information

--- src/org/freenetproject/freemail/ui/web/SettingsToadlet.java
+++ src/org/freenetproject/freemail/ui/web/SettingsToadlet.java
@@ -11,7 +11,10 @@ import org.freenetproject.freemail.l10n.FreemailL10n;
 
 import javax.naming.SizeLimitExceededException;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.HashMap;
 
 public class SettingsToadlet extends WebPage {
@@ -45,15 +48,28 @@ public class SettingsToadlet extends WebPage {
 
 	@Override
 	HTTPResponse makeWebPagePost(URI uri, HTTPRequest req, ToadletContext ctx, PageNode page) throws IOException {
-		// TODO: validate input data
+		String smtpPortStr = "";
+		String smtpAddressStr = "";
+		String imapPortStr = "";
+		String imapAddressStr = "";
+
 		try {
-			config.set("smtp_bind_port", req.getPartAsStringThrowing("smtp-bind-port", 5));
-			config.set("smtp_bind_address", req.getPartAsStringThrowing("smtp-bind-address", 2000));
-			config.set("imap_bind_port", req.getPartAsStringThrowing("imap-bind-port", 5));
-			config.set("imap_bind_address", req.getPartAsStringThrowing("imap-bind-address", 2000));
+			smtpPortStr = req.getPartAsStringThrowing("smtp-bind-port", 5);
+			smtpAddressStr = req.getPartAsStringThrowing("smtp-bind-address", 2000);
+			imapPortStr = req.getPartAsStringThrowing("imap-bind-port", 5);
+			imapAddressStr = req.getPartAsStringThrowing("imap-bind-address", 2000);
 		} catch (SizeLimitExceededException e) {
 			e.printStackTrace();
 		}
+
+		if (isPort(smtpPortStr))
+			config.set("smtp_bind_port", smtpPortStr);
+		if (isURL(smtpAddressStr))
+			config.set("smtp_bind_address", smtpAddressStr);
+		if (isPort(imapPortStr))
+			config.set("imap_bind_port", imapPortStr);
+		if (isURL(imapAddressStr))
+			config.set("imap_bind_address", imapAddressStr);
 
 		return makeWebPageGet(uri, req, ctx, page);
 	}
@@ -71,5 +87,27 @@ public class SettingsToadlet extends WebPage {
 	@Override
 	public String path() {
 		return PATH;
+	}
+
+	private static boolean isURL(String url) {
+		try {
+			new URL(url).toURI();
+			return true;
+		} catch (URISyntaxException | MalformedURLException exception) {
+			return false;
+		}
+	}
+
+	private static boolean isPort(String port) {
+		try {
+			int num = Integer.parseInt(port);
+
+			if(num > 0 && num <= 65535) {
+				return true;
+			}
+		} catch (NumberFormatException e) {
+			return false;
+		}
+		return false;
 	}
 }
