adc3df0406f7ec7b75c6f4a7986d452e78eccf80
Oleh <oleh.shklyar@gmail.com>
2019-06-16 22:03:27 +0300

    clear

--- src/org/freenetproject/freemail/FreemailPlugin.java
+++ src/org/freenetproject/freemail/FreemailPlugin.java
@@ -32,9 +32,9 @@ import org.freenetproject.freemail.l10n.FreemailL10n;
 import org.freenetproject.freemail.ui.web.WebInterface;
 import org.freenetproject.freemail.utils.Logger;
 import org.freenetproject.freemail.utils.Timer;
+import org.freenetproject.freemail.wot.ConcurrentWoTConnection;
 import org.freenetproject.freemail.wot.OwnIdentity;
 import org.freenetproject.freemail.wot.WoTConnection;
-import org.freenetproject.freemail.wot.WoTConnections;
 
 import freenet.l10n.BaseL10n.LANGUAGE;
 import freenet.pluginmanager.FredPlugin;
@@ -136,7 +136,7 @@ public class FreemailPlugin extends Freemail implements FredPlugin, FredPluginBa
 				return null;
 			}
 
-			wotConnection = WoTConnections.wotConnection(pluginRespirator);
+			wotConnection = new ConcurrentWoTConnection(pluginRespirator);
 		}
 		return wotConnection;
 	}
